62618aa9dce1058bbfa973617744caef2fb4b832
Tom <thomas.verin@sonarsource.com>
2016-04-13 11:45:28 +0200

    mvn install twice

--- cix.sh
+++ cix.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -euo pipefail
 echo "Running with SQ=$SQ_VERSION for $CI_BUILD_NUMBER"
+  # Need install because mvn org.sonarsource.scanner.maven:sonarqube-maven-plugin:<version>:sonar in ITs will take artifact from local repo
+  mvn install -B -e -V -Dsource.skip=true -Denforcer.skip=true -Danimal.sniffer.skip=true -Dmaven.test.skip=true
+
+  #deploy the version built by travis
   CURRENT_VERSION=`mvn help:evaluate -Dexpression="project.version" | grep -v '^\[\|Download\w\+\:'`
   RELEASE_VERSION=`echo $CURRENT_VERSION | sed "s/-.*//g"`
   NEW_VERSION="$RELEASE_VERSION-build$CI_BUILD_NUMBER"
@@ -9,9 +13,7 @@ echo "Running with SQ=$SQ_VERSION for $CI_BUILD_NUMBER"
   cd target
   curl --user $ARTIFACTORY_QA_READER_USERNAME:$ARTIFACTORY_QA_READER_PASSWORD -sSLO https://repox.sonarsource.com/sonarsource-public-qa/org/sonarsource/scanner/maven/sonar-maven-plugin/$NEW_VERSION/sonar-maven-plugin-$NEW_VERSION.jar
   cd ..
-
-  # Need install because mvn org.sonarsource.scanner.maven:sonarqube-maven-plugin:<version>:sonar in ITs will take artifact from local repo
-  #mvn install -B -e -V -Dsource.skip=true -Denforcer.skip=true -Danimal.sniffer.skip=true -Dmaven.test.skip=true
+ 
   mvn install:install-file -Dfile=target/sonar-maven-plugin-$NEW_VERSION.jar
 
 
